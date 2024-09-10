b65a331ed864d61dcb3eaa2fd88dffbd3f2b8280
fazlehaq <fazlehaqs360@gmail.com>
2024-09-10 12:11:38 +0530

    changed the embedder

--- embedder.py
+++ embedder.py
@@ -1,10 +1,18 @@
-from langchain_community.embeddings import TensorflowHubEmbeddings
+# from langchain_community.embeddings import TensorflowHubEmbeddings
+# embeddings = None
+# def getEmbedder():
+#     global embeddings
+#     if embeddings is None :
+#         model_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+#         embeddings = TensorflowHubEmbeddings(model_url=model_url) 
+#     return embeddings
+
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 embeddings = None
+load_dotenv()
 
 def getEmbedder():
     global embeddings
-    if embeddings is None :
-        model_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
-        embeddings = TensorflowHubEmbeddings(model_url=model_url) 
-        
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     return embeddings
